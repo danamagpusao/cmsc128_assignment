e9baf757851b55399279b5743f158ca4db8799f3
danamagpusao <danamagpusao818@gmail.com>
2016-02-14 08:46:21 +0800

    working numberDelimited.js

--- numberDelimited.js
+++ numberDelimited.js
@@ -1,6 +1,6 @@
 function addDelimiter(){
-	var num = document.getElementById("input").value;
-	var d = document.getElementById("cur").value;
+	var num = document.getElementById("number").value.split("");
+	var d = document.getElementById("del").value;
 	var offset = +(document.getElementById("offset").value);
 	if(d.length>1){
 		alert("Delimiter's length should be at most 1");
@@ -12,9 +12,25 @@ function addDelimiter(){
 }
 
 function numberDelimited(num, delimiter,offset){
-	for(var i=num.length-1;i>=0;i-=offset){
-			this.splice(i, 0, delimiter);
+	var chunks = [];
+	for(var i = num.length;i>=0;){
+		var s = "";
+		for(var j = 0 ; j<offset;j++){
+			i--;
+			if(i>=0) {s  = num[i] +""+ s;}
+		}
+		chunks.push(s);
 	}
-
+	var output = "";
+	for(var i = 0 ; i < chunks.length ; i++){
+		if(i != chunks.length-1)
+			{
+				output =  delimiter+ "" +chunks[i] + output;
+			}
+		else{
+			output =  "" +chunks[i] + output;
+		}
+	}
+	return output;
 }
 
